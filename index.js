e11ae5bf0a9c966a02d10413fc99103dbdccfead
Weston Rose <westonrose@Westons-MacBook-Air.local>
2020-02-20 13:02:40 -0600

    Test running my updated README code

--- index.js
+++ index.js
@@ -64,57 +64,51 @@ function writeToFile(fileName, data) {
     [![GitHub license](https://img.shields.io/badge/license-GPL%203.0-blue.svg)](https://github.com/${data.github_username}/${data.project_name})
 
 ## Description
-    ​
-        ${data.description}
-    ​
+${data.description}
 ## Table of Contents 
     ​
-        * [Installation](#installation)
-        ​
-        * [Usage](#usage)
-        ​
-        * [License](#license)
-        ​
-        * [Contributing](#contributing)
-        ​
-        * [Tests](#tests)
-        ​
-        * [Questions](#questions)
+* [Installation](#installation)
+  ​
+* [Usage](#usage)
+
+* [License](#license)
+
+* [Contributing](#contributing)
+
+* [Tests](#tests)
+ ​
+* [Questions](#questions)
     ​
 ## Installation
-    ​
-    To install necessary dependencies, run the following command:
-    ​
+To install necessary dependencies, run the following command:
 
-        ${data.dependencies}
-
+${data.dependencies}
     ​
 ## Usage
     ​
-        ${data.usingRepo}
+${data.usingRepo}
     ​
 ## License
     ​
-        This project is licensed under the ${data.licenses} license.
+This project is licensed under the ${data.licenses} license.
       
 ## Contributing
     ​
-        ${data.contributingRepo}
+${data.contributingRepo}
     ​
 ## Tests
     ​
-    To run tests, run the following command:
+To run tests, run the following command:
     ​
 
-        ${data.tests}
+${data.tests}
 
 
-        ​
 ## Questions
+
+[![User](${data.avatar_url})](${data.avatar_url})
     ​
-    <img src=${data.avatar_url}" alt="avatar" style="border-radius: 16px" width="30" />
-    ​
-    If you have any questions about the repo, open an issue or contact [${data.github_username}](https://api.github.com/users/${data.login}).
+If you have any questions about the repo, open an issue or contact [${data.github_username}](https://api.github.com/users/${data.login}).
     
     `
     fs.writeFile(fileName, fileContents, function (err) {
--- output/README.md
+++ output/README.md
@@ -1,38 +1,50 @@
-# nothing
-[![GitHub license](https://img.shields.io/badge/license-GPL%203.0-blue.svg)](https://github.com/wrose001/nothing)
+# Manhattan Project
+    [![GitHub license](https://img.shields.io/badge/license-GPL%203.0-blue.svg)](https://github.com/wrose001/Manhattan Project)
 
 ## Description
-nothing 1
+This is a description
+## Table of Contents 
     ​
-## Table of Contents  ​
 * [Installation](#installation)
-        ​
+  ​
 * [Usage](#usage)
-        ​
+
 * [License](#license)
-        ​
+
 * [Contributing](#contributing)
-        ​
+
 * [Tests](#tests)
-        ​
+ ​
 * [Questions](#questions)
     ​
-## Installation​
+## Installation
 To install necessary dependencies, run the following command:
-dkfjhsdklf
+
+None
+    ​
 ## Usage
-dfsdf
-## License ​
+    ​
+Nothing, reall
+    ​
+## License
+    ​
 This project is licensed under the ISC license.
       
-## Contributing ​
-sdfd
-## Tests​
-To run tests, run the following command:  ​
+## Contributing
+    ​
+Nada
+    ​
+## Tests
+    ​
+To run tests, run the following command:
+    ​
 
-sdkfjsdk
+All of them
+
+
 ## Questions
-[![User](https://avatars0.githubusercontent.com/u/55845463?v=4/)](https://avatars0.githubusercontent.com/u/55845463?v=4/)
+
+[![User](https://avatars0.githubusercontent.com/u/55845463?v=4)](https://avatars0.githubusercontent.com/u/55845463?v=4)
     ​
 If you have any questions about the repo, open an issue or contact [wrose001](https://api.github.com/users/undefined).
     
