402fc746cfaacaeef4022364b1b1fb350e9f84d9
Weston Rose <westonrose@Westons-MacBook-Air.local>
2020-02-20 18:13:07 -0600

    Added defaults to questions array

--- index.js
+++ index.js
@@ -38,12 +38,14 @@ const questions = [
     {
         type: "input",
         name: "dependencies",
-        message: "What command should be run to install dependencies?" 
+        message: "What command should be run to install dependencies?",
+        default: "npm i"   
     },
     {
         type: "input",
         name: "tests",
-        message: "What command should be run to run tests?" 
+        message: "What command should be run to run tests?",
+        default: "npm test"   
     },
     {
         type: "input",
